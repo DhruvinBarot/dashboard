5b157dc88c0225c6a2ec6b352d54df27044bafc7
DhruvinBarot <101793709+DhruvinBarot@users.noreply.github.com>
2023-06-21 17:48:01 +0530

    Add files via upload

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,31 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const StateContext = createContext();
+
+const initialState = {
+    cart : false,
+    chat : false,
+    userProfile : false,
+    notification : false,
+};
+
+export const ContextProvider = ({ children }) => {
+    const [activeMenu, setActiveMenu] = useState(true);
+    const [isClicked, setIsClicked] = useState(initialState);
+    const handleClick = (clicked) => {
+        setIsClicked({ ...initialState, [clicked] : true});
+    }
+
+    const [screenSize, setScreenSize] = useState(undefined);
+
+        return (
+        <StateContext.Provider value = {{ 
+            activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize
+                       }}
+        >
+            {children}
+        </StateContext.Provider>
+        );
+};
+
+export const useStateContext = () => useContext(StateContext);
